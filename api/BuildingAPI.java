d70645c6fe0d1971d2ccd0d6e72ea94f24aec17f
fatherOfChildren568 <contact.me.dothehung@gmail.com>
2025-08-29 04:21:54 +0700

    get param by map

--- api/BuildingAPI.java
+++ api/BuildingAPI.java
@@ -1,5 +1,7 @@
 package com.javaweb.api;
 
+import java.util.Map;
+
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RequestMethod;
@@ -10,13 +12,19 @@ import org.springframework.web.bind.annotation.RequestParam;
 
 public class BuildingAPI {
 	@RequestMapping(value = "/api/building/", method = RequestMethod.GET)
-	// add paramerter required=false. The default of this paremeter is true.
+	// add parameter required=false. The default of this parameter is true.
 	// it means any variable in request param must has value
-	// when set it is false, it dont need required any variable in request
+	// when set it is false, it don't need required any variable in request
 	// must has value, so it can null
 	public void getBuilding(@RequestParam(value = "name", required = false) String name,
 			@RequestParam(value = "size", required = false) String size,
 			@RequestParam(value = "floor", required = false) Integer floor) {
 		System.out.println(name + " " + size + " " + floor);
+	}
+
+	// get data by Map
+	@RequestMapping(value = "/api/building/", method = RequestMethod.POST)
+	public void getBuilding2(@RequestParam Map<String, String> params) {
+		System.out.println("ok");
 	}
 }
