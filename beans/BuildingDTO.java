6183eb1c2203477739bdcfbfaed932d85679584a
fatherOfChildren568 <contact.me.dothehung@gmail.com>
2025-08-29 14:57:16 +0700

    create Java Beans

---
+++ beans/BuildingDTO.java
@@ -0,0 +1,50 @@
+package com.javaweb.beans;
+
+public class BuildingDTO {
+	private String name;
+	private String size;
+	private int floor;
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the size
+	 */
+	public String getSize() {
+		return size;
+	}
+
+	/**
+	 * @param size the size to set
+	 */
+	public void setSize(String size) {
+		this.size = size;
+	}
+
+	/**
+	 * @return the floor
+	 */
+	public int getFloor() {
+		return floor;
+	}
+
+	/**
+	 * @param floor the floor to set
+	 */
+	public void setFloor(int floor) {
+		this.floor = floor;
+	}
+
+}
