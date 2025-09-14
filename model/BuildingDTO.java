b10f5073da0156b7499cf89fcd75aec8fc09fb4b
fatherOfChildren568 <contact.me.dothehung@gmail.com>
2025-09-14 13:16:48 +0700

    change package beans -> model

---
+++ model/BuildingDTO.java
@@ -0,0 +1,65 @@
+package com.javaweb.model;
+
+public class BuildingDTO {
+	private String name;
+	private String size;
+	private int floor;
+	private String address;
+
+	/**
+	 * @return the name
+	 */
+	public String getName() {
+		return name;
+	}
+
+	/**
+	 * @param name the name to set
+	 */
+	public void setName(String name) {
+		this.name = name;
+	}
+
+	/**
+	 * @return the size
+	 */
+	public String getSize() {
+		return size;
+	}
+
+	/**
+	 * @param size the size to set
+	 */
+	public void setSize(String size) {
+		this.size = size;
+	}
+
+	/**
+	 * @return the floor
+	 */
+	public int getFloor() {
+		return floor;
+	}
+
+	/**
+	 * @param floor the floor to set
+	 */
+	public void setFloor(int floor) {
+		this.floor = floor;
+	}
+
+	/**
+	 * @return the address
+	 */
+	public String getAddress() {
+		return address;
+	}
+
+	/**
+	 * @param address the address to set
+	 */
+	public void setAddress(String address) {
+		this.address = address;
+	}
+
+}
